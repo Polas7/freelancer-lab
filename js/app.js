d3c283ae3a384860f95a2ee57301a100feeffc31
Pola <Pola.kinomoto@gmail.com>
2017-06-02 22:13:59 -0400

    funcionalidad js

---
+++ js/app.js
@@ -0,0 +1,48 @@
+/*validacion del formulario*/
+var boton = document.getElementById("boton");
+boton.addEventListener('click', validar);
+
+ function validar() {
+ 	var name = document.getElementById("name");
+ 	var email = document.getElementById("email");
+ 	var phoneNumber = document.getElementById("phone");
+ 	var message = document.getElementById("textarea");
+ 	var contenido = "";
+    if (name.value === "") {
+    	name.value = "● Please enter your name.";
+    }
+    if (email.value === "") {
+    	email.value = "● Please enter your email.";
+    }
+    if (phone.value === "") {
+    	phone.value = "● Please enter your phone number.";
+    }
+    if (mensaje.value === "") {
+    	mensaje.value = "● Please enter your message.";
+    }
+};
+/*modal*/
+(function main(){
+	var boxes = Array.from(document.getElementsByClassName("box-services"));
+	var modal = document.getElementById("box-services-modal");
+	var bodyModal, close, img;
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			modal.innerHTML = "";
+			bodyModal = document.createElement("div");
+			bodyModal.classList.add("modal-body");
+			bodyModal.innerHTML = box.innerHTML;			
+			modal.appendChild(bodyModal);
+			modal.classList.remove("hide");
+			close = document.createElement("div");
+			close.classList.add("close");
+			img = document.createElement("img");
+			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+			close.appendChild(img);
+			modal.appendChild(close);
+			close.addEventListener("click",function(){
+				modal.classList.add("hide");
+			});
+		});		
+	});
+})();
